8f24bdd5cce5472050bf991bd3bab521d4c5bd86
seoyeon83 <mool848777@naver.com>
2024-07-24 15:59:11 +0900

    feat: 변수 활용 방식 개선

--- dag/extract_kakaomap.py
+++ dag/extract_kakaomap.py
@@ -2,17 +2,42 @@ import requests
 
 url = 'https://dapi.kakao.com/v2/local/search/keyword.json'
 headers = {"Authorization": "KakaoAK 0b802f8cc02b1b1343b8706eaf18efd1"}
-category_group_code = {"대형마트":"MT1",
-                        "편의점":"CS2",
-                        "지하철역":"SW8",
-                        "은행":"BK9",
-                        "음식점":"FD6",
-                        "카페":"CE7",
-                        "병원":"HP8",
-                        "약국":"PM9"}
+category_group = {
+                    "대형마트":{
+                        "code":"MT1",
+                        "radius":1712,
+                        "eng":"marcket"
+                    },
+                    "편의점":{
+                        "code":"CS2",
+                        "radius":629,
+                        "eng":"store"
+                    },
+                    "지하철역":{
+                        "code":"SW8",
+                        "radius":1058,
+                        "eng":"subway"
+                    },
+                    "음식점":{
+                        "code":"FD6",
+                        "radius":500,
+                        "eng":"restaurant"
+                    },
+                    "카페":{
+                        "code":"CE7",
+                        "radius":987,
+                        "eng":"cafe"
+                    },
+                    "병원":{
+                        "code":"HP8",
+                        "radius":946,
+                        "eng":"hospital"
+                    }
+                }
 
-def extract_nearest_facilities_info(lng, lat, category, radius=500) -> dict:
-    params = {'query' : category, 'x' : lng, 'y' : lat, 'radius' : radius, 'category_group_code' : category_group_code[category]}
+
+def extract_nearest_facilities_info(lng, lat, category) -> dict:
+    params = {'query' : category, 'x' : lng, 'y' : lat, 'radius' : category_group[category]["radius"], 'category_group_code' : category_group[category]["code"]}
     
     response = requests.get(url, params=params, headers=headers)
     
@@ -20,7 +45,7 @@ def extract_nearest_facilities_info(lng, lat, category, radius=500) -> dict:
         data = response.json()
         count = data['meta']['total_count']
         if count > 0:
-            nearest_distance = data["documents"][0]["distance"]  
+            nearest_distance = int(data["documents"][0]["distance"])
         else: 
             nearest_distance = None
         
@@ -30,4 +55,3 @@ def extract_nearest_facilities_info(lng, lat, category, radius=500) -> dict:
         print("Error:", response.json())
         print(error)
         raise
-
