1706d58980d0e4f7dba649a66bc40da1b8e5a890
Hyoungwon <skqltldnjf77@gmail.com>
2024-08-02 13:08:29 +0900

    Update dabang_csv_to_s3.py

--- dags/dabang_csv_to_s3.py
+++ dags/dabang_csv_to_s3.py
@@ -9,7 +9,7 @@ import time
 
 def fetch_data():
     import extract_dabang_v2
-    extract_dabang_v2.get_data_all()
+    extract_dabang_v2.get_data_range(1,2)
 
 
 def upload_to_s3(filename: str, key: str, bucket_name: str) -> None:
@@ -36,7 +36,7 @@ with DAG(
         task_id="save_upload",
         python_callable=upload_to_s3,
         op_kwargs={
-            filename": "/opt/airflow/data/dabang.parquet",
+            "filename": "/opt/airflow/data/dabang.parquet",
             "key": "dabang/save/dabang_{{ ds }}.parquet",
             "bucket_name": "team-ariel-1-bucket",
         },
