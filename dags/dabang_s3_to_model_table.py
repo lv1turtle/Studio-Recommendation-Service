53f45135c86d1811c94bb27a6b8225ae33ea5b72
이정근 <97031069+nooreong2@users.noreply.github.com>
2024-08-05 19:05:13 +0900

    [ENH] dabang_s3_to_model_table.py
    S3 connect 문제 해결
    파일이 존재하지 않는 경우 예외처리
    실행하는 날짜에 맞춰 parquet파일 처리
    
    Redshift 테이블 생성 후, 테이블 이름 변경 필요

--- dags/dabang_s3_to_model_table.py
+++ dags/dabang_s3_to_model_table.py
@@ -1,17 +1,15 @@
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.python import PythonOperator
-from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
-from airflow.models import Variable
-from airflow import DAG
-
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.models import DAG
+
+from datetime import datetime, timedelta
 import pandas as pd
 import pyarrow.parquet as pq
-import os
-from io import BytesIO
-from datetime import datetime, timedelta
 import logging
-
+from io import BytesIO
+from botocore.exceptions import ClientError
+from airflow.exceptions import AirflowFailException
 
 # Redshift 연결
 def get_redshift_conn(autocommit=True):
@@ -20,14 +18,25 @@ def get_redshift_conn(autocommit=True):
     conn.autocommit = autocommit
     return conn.cursor()
 
-
 # S3에서 parquet 파일 읽기
-def read_parquet_from_s3(bucket_name, key):
-    s3 = S3Hook("s3_conn")
-    s3_client = s3.get_client_type("s3")
-    obj = s3_client.get_object(Bucket=bucket_name, Key=key)
-    return pq.read_table(BytesIO(obj["Body"].read())).to_pandas()
+def read_parquet_from_s3(bucket_name: str, key: str):
 
+    s3 = S3Hook(aws_conn_id="s3_conn")
+    s3_client = s3.get_conn()
+
+    try:
+        obj = s3_client.get_object(Bucket=bucket_name, Key=key)
+        return pq.read_table(BytesIO(obj["Body"].read())).to_pandas()
+
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'NoSuchKey':
+            error_message =f"{key}에 해당하는 파일이 {bucket_name}에 존재하지 않습니다."
+            logging.error(error_message)
+            raise AirflowFailException(error_message)
+        else:
+            error_message = f"An error occurred: {e}"
+            logging.error(error_message)
+            raise AirflowFailException(error_message)
 
 # 두 parquet 파일 비교
 def compare_parquet_files(bucket_name, key1, key2):
@@ -37,8 +46,14 @@ def compare_parquet_files(bucket_name, key1, key2):
     # df1에는 있고 df2에는 없는 데이터를 추출
     missing_data = df1[~df1.isin(df2.to_dict(orient="list")).all(axis=1)]
 
-    return missing_data
+    columns_to_keep = ['room_id', 'floor', 'area', 'subway_count', 'store_count', 'cafe_count', 
+                    'market_count', 'restaurant_count', 'hospital_count', 'deposit', 
+                    'rent', 'maintenance_fee', 'address']
+    
+    missing_data = missing_data[columns_to_keep]
+    missing_data['status'] = 0
 
+    return missing_data
 
 # Redshift에 데이터 저장
 def save_to_redshift(data, table_name):
@@ -54,13 +69,19 @@ def save_to_redshift(data, table_name):
 
     cursor.close()
 
-
-# 메인 작업 함수
 def compare_and_save(**kwargs):
-    bucket_name = "team-ariel-1-bucket"
-    key1 = "dabang/save/2024-08-02/dabang_2024-08-02.parquet"
-    key2 = "dabang/save/2024-08-03/dabang_2024-08-03.parquet"
+    executionDate = kwargs['ds']
+    execution_date = datetime.strptime(executionDate, '%Y-%m-%d')
+    execution_date_yesterday = execution_date - timedelta(days=1)
 
+    execution_date_str = execution_date.strftime('%Y-%m-%d')
+    execution_date_yesterday_str = execution_date_yesterday.strftime('%Y-%m-%d')
+    
+    bucket_name = "team-ariel-1-bucket"
+
+    key1 = f"dabang/save/{execution_date_str}/dabang_{execution_date_str}.parquet"
+    key2 = f"dabang/save/{execution_date_yesterday_str}/dabang_{execution_date_yesterday_str}.parquet"
+    
     # 두 파일 비교
     missing_data = compare_parquet_files(bucket_name, key1, key2)
 
@@ -68,7 +89,8 @@ def compare_and_save(**kwargs):
     if not missing_data.empty:
         # save_to_redshift(missing_data, "your_redshift_table_name")
         logging.info("Missing data:\n%s", missing_data)
-
+    else:
+        logging.info("No missing data found.")
 
 # DAG 설정
 default_args = {
@@ -80,20 +102,15 @@ default_args = {
     "retry_delay": timedelta(minutes=5),
 }
 
-dag = DAG(
+with DAG(
     "s3_parquet_compare",
     default_args=default_args,
     description="Compare two parquet files in S3 and save missing data to Redshift",
     schedule_interval=timedelta(days=1),
     start_date=datetime(2023, 8, 1),
     catchup=False,
-)
-
-compare_and_save_task = PythonOperator(
-    task_id="compare_and_save_task",
-    python_callable=compare_and_save,
-    provide_context=True,
-    dag=dag,
-)
-
-compare_and_save_task
+) as dag:
+    compare_and_save_task = PythonOperator(
+        task_id="compare_and_save_task",
+        python_callable=compare_and_save,
+    )
