c2d5c3fc8e9411f7605b35f2312b2ffc6e8e1117
nooreong2 <nooreong0503@gmail.com>
2024-08-08 18:40:41 +0900

    test variable

--- dags/dabang_csv_to_s3.py
+++ dags/dabang_csv_to_s3.py
@@ -9,12 +9,16 @@ import time
 
 def fetch_data():
     import extract_dabang_v2
-    extract_dabang_v2.get_data_all()
+
+    # extract_dabang_v2.get_data_all()
+    extract_dabang_v2.get_data_by_range(1, 2)
 
 
 def upload_to_s3(filename: str, key: str, bucket_name: str) -> None:
     hook = S3Hook("s3_conn")
-    hook.load_file(filename=filename, key=key, bucket_name=bucket_name, replace=True)
+    hook.load_file(
+        filename=filename, key=key, bucket_name=bucket_name, replace=True
+    )
 
 
 def clear_data(filename: str) -> None:
@@ -37,7 +41,7 @@ with DAG(
         python_callable=upload_to_s3,
         op_kwargs={
             "filename": "/opt/airflow/data/dabang.parquet",
-            "key": "dabang/save/{{ ds }}/dabang_{{ ds }}.parquet",
+            "key": "dabang/save/{{ ds }}/dabang_{{ ds }}_test.parquet",
             "bucket_name": "team-ariel-1-bucket",
         },
     )
@@ -46,7 +50,7 @@ with DAG(
         python_callable=upload_to_s3,
         op_kwargs={
             "filename": "/opt/airflow/data/dabang.parquet",
-            "key": "dabang/overwrite/dabang.parquet",
+            "key": "dabang/overwrite/dabang_test.parquet",
             "bucket_name": "team-ariel-1-bucket",
         },
     )
