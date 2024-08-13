04a7c55850a51429f5c5fb6e2e19a97cc97868ad
seoyeon83 <mool848777@naver.com>
2024-08-14 02:11:58 +0900

    feat: 예외처리 추가

--- dags/ml_pipeline.py
+++ dags/ml_pipeline.py
@@ -10,17 +10,25 @@ from airflow.providers.mysql.hooks.mysql import MySqlHook
 
 
 def get_Redshift_connection(autocommit=True):
-    hook = PostgresHook(postgres_conn_id='redshift_conn')  # redshift_dev_db
-    conn = hook.get_conn()
-    conn.autocommit = autocommit
-    return conn.cursor()
+    try:
+        hook = PostgresHook(postgres_conn_id='redshift_conn')
+        conn = hook.get_conn()
+        conn.autocommit = autocommit
+        return conn.cursor()
+    except Exception as error:
+        print(f"Error connecting to Redshift: {error}")
+        raise
 
 
 def get_RDS_connection(autocommit=True):
-    hook = MySqlHook(mysql_conn_id='rds_conn', local_infile=True)
-    conn = hook.get_conn()
-    conn.autocommit = autocommit
-    return conn.cursor()
+    try:
+        hook = MySqlHook(mysql_conn_id='rds_conn', local_infile=True)
+        conn = hook.get_conn()
+        conn.autocommit = autocommit
+        return conn.cursor()
+    except Exception as error:
+        print(f"Error connecting to RDS: {error}")
+        raise
 
 
 # redshift 에서 sold 테이블에 대해 전처리 후 전처리 테이블 생성
@@ -103,7 +111,7 @@ def preprocessing_redshift_sold_table(schema, raw_table, preprocessed_table): # 
         cursor.execute("COMMIT;") 
 
     except Exception as error:
-        print(error)
+        print(f"Error preprocessing data from Redshift: {error}")
         print("ROLLBACK")
         cursor.execute("ROLLBACK;")
         raise
@@ -114,14 +122,18 @@ def preprocessing_redshift_sold_table(schema, raw_table, preprocessed_table): # 
 
 # 전처리 테이블에서 데이터를 가져와 데이터프레임 형태로 return
 def fetch_preprocessed_data_from_redshift(schema, preprocessed_table):
-    cursor = get_Redshift_connection()  
+    try:
+        cursor = get_Redshift_connection()  
 
-    query = f"SELECT * FROM {schema}.{preprocessed_table};"
-    cursor.execute(query)
+        query = f"SELECT * FROM {schema}.{preprocessed_table};"
+        cursor.execute(query)
 
-    records = cursor.fetchall()
-    
-    cursor.close()
+        records = cursor.fetchall()
+    except Exception as error:
+        print(f"Error fetching preprocessed data from Redshift: {error}")
+        raise
+    finally:
+        cursor.close()
 
     columns = ["room_id", "floor_level", "area", "deposit", "rent", "maintenance_fee", "district", "facility_count", "status"]
     df = pd.DataFrame(records, columns=columns)
@@ -161,15 +173,19 @@ def perform_undersampling(df):
 
 # random forest 모델 학습 및 accuracy 도출
 def train_randomforest(df):
-    X = df.drop(columns=['room_id', 'status'])
-    y = df['status']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    try:
+        X = df.drop(columns=['room_id', 'status'])
+        y = df['status']
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    clf = RandomForestClassifier()
-    clf.fit(X_train, y_train)
+        clf = RandomForestClassifier()
+        clf.fit(X_train, y_train)
 
-    y_pred = clf.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)\
+        y_pred = clf.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+    except Exception as error:
+        print(f"Error during model training: {error}")
+        raise
     
     return clf, accuracy
 
@@ -192,7 +208,7 @@ def insert_accuracy_to_redshift(schema, table, execution_date, accuracy):
         cursor.execute("COMMIT;") 
 
     except Exception as error:
-        print(error)
+        print(f"Error inserting accuracy to redshift: {error}")
         print("ROLLBACK")
         cursor.execute("ROLLBACK;")
         raise
@@ -202,82 +218,87 @@ def insert_accuracy_to_redshift(schema, table, execution_date, accuracy):
     
 
 # RDS에서 데이터를 가져오면서 전처리 작업
-def fetch_preprocessed_data_from_rds(schema, table): # taw_data.property
-    cursor = get_RDS_connection()  
+def fetch_preprocessed_data_from_rds(schema, table):
+    try:
+        cursor = get_RDS_connection()  
 
-    query = f"""
-        SELECT *
-        FROM (
-            SELECT room_id,
-                CASE
-                    WHEN floor LIKE '%옥탑%' THEN '옥탑'
-                    WHEN floor LIKE '%반지%' THEN '반지하'
-                    WHEN floor LIKE '%저%' THEN '기타'
-                    WHEN floor LIKE '%중%' THEN '기타'
-                    WHEN floor LIKE '%고%' THEN '기타'
-                    WHEN floor LIKE '%층' THEN
-                        CASE
-                            WHEN CAST(REPLACE(floor, '층', '') AS SIGNED) BETWEEN 1 AND 2 THEN '저'
-                            WHEN CAST(REPLACE(floor, '층', '') AS SIGNED) BETWEEN 3 AND 14 THEN '중'
-                            WHEN CAST(REPLACE(floor, '층', '') AS SIGNED) >= 15 THEN '고'
-                        END
-                    WHEN floor NOT LIKE '%층' THEN
-                        CASE
-                            WHEN CAST(floor AS SIGNED) BETWEEN 1 AND 2 THEN '저'
-                            WHEN CAST(floor AS SIGNED) BETWEEN 3 AND 14 THEN '중'
-                            WHEN CAST(floor AS SIGNED) >= 15 THEN '고'
-                        END
-                    ELSE '기타'
-                END AS floor_level,
-                area,
-                deposit,
-                rent,
-                maintenance_fee,
-                CASE
-                    WHEN address LIKE '%강남구%' THEN '강남구'
-                    WHEN address LIKE '%강동구%' THEN '강동구'
-                    WHEN address LIKE '%강북구%' THEN '강북구'
-                    WHEN address LIKE '%강서구%' THEN '강서구'
-                    WHEN address LIKE '%관악구%' THEN '관악구'
-                    WHEN address LIKE '%광진구%' THEN '광진구'
-                    WHEN address LIKE '%구로구%' THEN '구로구'
-                    WHEN address LIKE '%금천구%' THEN '금천구'
-                    WHEN address LIKE '%노원구%' THEN '노원구'
-                    WHEN address LIKE '%도봉구%' THEN '도봉구'
-                    WHEN address LIKE '%동대문구%' THEN '동대문구'
-                    WHEN address LIKE '%동작구%' THEN '동작구'
-                    WHEN address LIKE '%마포구%' THEN '마포구'
-                    WHEN address LIKE '%서대문구%' THEN '서대문구'
-                    WHEN address LIKE '%서초구%' THEN '서초구'
-                    WHEN address LIKE '%성동구%' THEN '성동구'
-                    WHEN address LIKE '%성북구%' THEN '성북구'
-                    WHEN address LIKE '%송파구%' THEN '송파구'
-                    WHEN address LIKE '%양천구%' THEN '양천구'
-                    WHEN address LIKE '%영등포구%' THEN '영등포구'
-                    WHEN address LIKE '%용산구%' THEN '용산구'
-                    WHEN address LIKE '%은평구%' THEN '은평구'
-                    WHEN address LIKE '%종로구%' THEN '종로구'
-                    WHEN address LIKE '%중구%' THEN '중구'
-                    WHEN address LIKE '%중랑구%' THEN '중랑구'
-                    ELSE '기타'
-                END AS district,
-                CAST(
-                    (CASE WHEN subway_count >= 1 THEN 1 ELSE 0 END +
-                    CASE WHEN store_count >= 1 THEN 1 ELSE 0 END +
-                    CASE WHEN cafe_count >= 1 THEN 1 ELSE 0 END +
-                    CASE WHEN market_count >= 1 THEN 1 ELSE 0 END +
-                    CASE WHEN restaurant_count >= 1 THEN 1 ELSE 0 END +
-                    CASE WHEN hospital_count >= 1 THEN 1 ELSE 0 END)
-                AS SIGNED) AS facility_count
-            FROM {schema}.{table}
-        ) AS subquery
-        WHERE district <> '기타' AND floor_level <> '기타';
-    """
-    cursor.execute(query)
+        query = f"""
+            SELECT *
+            FROM (
+                SELECT room_id,
+                    CASE
+                        WHEN floor LIKE '%옥탑%' THEN '옥탑'
+                        WHEN floor LIKE '%반지%' THEN '반지하'
+                        WHEN floor LIKE '%저%' THEN '기타'
+                        WHEN floor LIKE '%중%' THEN '기타'
+                        WHEN floor LIKE '%고%' THEN '기타'
+                        WHEN floor LIKE '%층' THEN
+                            CASE
+                                WHEN CAST(REPLACE(floor, '층', '') AS SIGNED) BETWEEN 1 AND 2 THEN '저'
+                                WHEN CAST(REPLACE(floor, '층', '') AS SIGNED) BETWEEN 3 AND 14 THEN '중'
+                                WHEN CAST(REPLACE(floor, '층', '') AS SIGNED) >= 15 THEN '고'
+                            END
+                        WHEN floor NOT LIKE '%층' THEN
+                            CASE
+                                WHEN CAST(floor AS SIGNED) BETWEEN 1 AND 2 THEN '저'
+                                WHEN CAST(floor AS SIGNED) BETWEEN 3 AND 14 THEN '중'
+                                WHEN CAST(floor AS SIGNED) >= 15 THEN '고'
+                            END
+                        ELSE '기타'
+                    END AS floor_level,
+                    area,
+                    deposit,
+                    rent,
+                    maintenance_fee,
+                    CASE
+                        WHEN address LIKE '%강남구%' THEN '강남구'
+                        WHEN address LIKE '%강동구%' THEN '강동구'
+                        WHEN address LIKE '%강북구%' THEN '강북구'
+                        WHEN address LIKE '%강서구%' THEN '강서구'
+                        WHEN address LIKE '%관악구%' THEN '관악구'
+                        WHEN address LIKE '%광진구%' THEN '광진구'
+                        WHEN address LIKE '%구로구%' THEN '구로구'
+                        WHEN address LIKE '%금천구%' THEN '금천구'
+                        WHEN address LIKE '%노원구%' THEN '노원구'
+                        WHEN address LIKE '%도봉구%' THEN '도봉구'
+                        WHEN address LIKE '%동대문구%' THEN '동대문구'
+                        WHEN address LIKE '%동작구%' THEN '동작구'
+                        WHEN address LIKE '%마포구%' THEN '마포구'
+                        WHEN address LIKE '%서대문구%' THEN '서대문구'
+                        WHEN address LIKE '%서초구%' THEN '서초구'
+                        WHEN address LIKE '%성동구%' THEN '성동구'
+                        WHEN address LIKE '%성북구%' THEN '성북구'
+                        WHEN address LIKE '%송파구%' THEN '송파구'
+                        WHEN address LIKE '%양천구%' THEN '양천구'
+                        WHEN address LIKE '%영등포구%' THEN '영등포구'
+                        WHEN address LIKE '%용산구%' THEN '용산구'
+                        WHEN address LIKE '%은평구%' THEN '은평구'
+                        WHEN address LIKE '%종로구%' THEN '종로구'
+                        WHEN address LIKE '%중구%' THEN '중구'
+                        WHEN address LIKE '%중랑구%' THEN '중랑구'
+                        ELSE '기타'
+                    END AS district,
+                    CAST(
+                        (CASE WHEN subway_count >= 1 THEN 1 ELSE 0 END +
+                        CASE WHEN store_count >= 1 THEN 1 ELSE 0 END +
+                        CASE WHEN cafe_count >= 1 THEN 1 ELSE 0 END +
+                        CASE WHEN market_count >= 1 THEN 1 ELSE 0 END +
+                        CASE WHEN restaurant_count >= 1 THEN 1 ELSE 0 END +
+                        CASE WHEN hospital_count >= 1 THEN 1 ELSE 0 END)
+                    AS SIGNED) AS facility_count
+                FROM {schema}.{table}
+            ) AS subquery
+            WHERE district <> '기타' AND floor_level <> '기타';
+        """
+        cursor.execute(query)
 
-    records = cursor.fetchall()
+        records = cursor.fetchall()
     
-    cursor.close()
+    except Exception as error:
+        print(f"Error fetching data from RDS: {error}")
+        raise
+    finally:
+        cursor.close()
 
     columns = ["room_id", "floor_level", "area", "deposit", "rent", "maintenance_fee", "district", "facility_count"]
     df = pd.DataFrame(records, columns=columns)
@@ -294,24 +315,21 @@ def predict_status(df, model):
 
 
 def update_status_in_rds(df, schema, table):
-    update_query = f"""
-            UPDATE {schema}.{table}
-            SET status = %s
-            WHERE room_id = %s
-        """
-    
     try:
         cursor = get_RDS_connection()  
-
+        update_query = f"""
+                    UPDATE {schema}.{table}
+                    SET status = %s
+                    WHERE room_id = %s
+                """
+        
         cursor.execute("BEGIN;")
-
         for _, row in df.iterrows():
             cursor.execute(update_query, (row["room_id"], row["status"]))
-        
         cursor.execute("COMMIT;") 
 
     except Exception as error:
-        print(error)
+        print(f"Error updating status to RDS: {error}")
         print("ROLLBACK")
         cursor.execute("ROLLBACK;")
         raise
