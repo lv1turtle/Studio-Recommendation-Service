302acf64a54d0384d890c66241513a2d14d50f67
nooreong2 <nooreong0503@gmail.com>
2024-08-15 14:51:32 +0900

    test Xcom

--- dags/dabang_csv_to_s3.py
+++ dags/dabang_csv_to_s3.py
@@ -1,25 +1,33 @@
-from datetime import datetime, timedelta
-
+from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
-
-import time
 
 
 def fetch_data():
     import extract_dabang_v2
 
-    extract_dabang_v2.get_data_all()
+    output_file = extract_dabang_v2.get_data_by_range(1, 2)
+    kwargs["ti"].xcom_push(key="output_file", value=output_file)
 
 
-def upload_to_s3(filename: str, key: str, bucket_name: str) -> None:
-    import os
+def upload_to_s3(**kwargs):
     hook = S3Hook("s3_conn")
+
+    ti = kwargs["ti"]
+    filename = ti.xcom_pull(task_ids="fetch_data", key="output_file")
+
+    if not filename:
+        raise ValueError("No file found to upload.")
+
+    # S3로 파일 업로드
     hook.load_file(
-        filename=filename, key=key, bucket_name=bucket_name, replace=True
+        filename=filename,
+        key=kwargs["key"],
+        bucket_name=kwargs["bucket_name"],
+        replace=True,
     )
-    os.remove(filename)
+
 
 with DAG(
     "dabang_upload_to_s3",
@@ -28,7 +36,9 @@ with DAG(
     catchup=False,
 ) as dag:
 
-    fetch = PythonOperator(task_id="fetch_data", python_callable=fetch_data)
+    fetch = PythonOperator(
+        task_id="fetch_data", python_callable=fetch_data, provide_context=True
+    )
 
     save_upload = PythonOperator(
         task_id="save_upload",
@@ -38,6 +48,7 @@ with DAG(
             "key": "dabang/save/{{ ds }}/dabang_{{ ds }}.parquet",
             "bucket_name": "team-ariel-1-bucket",
         },
+        provide_context=True,
     )
     overwrite_upload = PythonOperator(
         task_id="overwrite_upload",
@@ -47,6 +58,7 @@ with DAG(
             "key": "dabang/overwrite/dabang.parquet",
             "bucket_name": "team-ariel-1-bucket",
         },
+        provide_context=True,
     )
 
 fetch >> overwrite_upload >> save_upload
